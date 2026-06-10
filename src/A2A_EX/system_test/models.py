b132dae53117009dcaee5e68263448e5fd51610c
1801573781 <1801573781@qq.com>
2026-06-11 06:14:28 +0800

    sth.

--- src/A2A_EX/system_test/models.py
+++ src/A2A_EX/system_test/models.py
@@ -98,6 +98,7 @@ class UnitTestCase(BaseModel):
     target_module: str = ""
     target_function: str = ""
     test_input: str = ""
+    reasoning_input: str = ""
     expected_behavior: str = ""
     check_type: str = "general"
 
